cea90f99100bca04b755b4ae8c6aef4aad1da908
Mac Rowe <m.rowe42@gmail.com>
2021-02-25 14:32:28 -0500

    commit tree 49d81c3c804d46c4a5fb061e18dabca29051597e

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
 
+def my_collect(words)
 
+    i = 0
+    collect = []
+    
+    while i < words.length
+        collect << yield(words[i])
+        i = i + 1
+      end
+    return collect
+  
+  end
+  
